db5f6afa5c2fa9523d408f96ce9c43f0a8e2f3f2
Masaki Hara <ackie.h.gmai@gmail.com>
2015-08-19 12:38:40 +0900

    move Color.cpp to Color.c.

--- accordion/Color.c
+++ accordion/Color.c
@@ -5,26 +5,26 @@ struct Color {
   double red, green, blue, alpha;
 };
 
-static Color *convertColor(VALUE obj);
-static void color_mark(Color *);
+static struct Color *convertColor(VALUE obj);
+static void color_mark(struct Color *);
 static VALUE color_alloc(VALUE klass);
 
 VALUE rb_color_new(double red, double green, double blue, double alpha) {
   VALUE ret = color_alloc(rb_cColor);
-  Color *ptr = convertColor(ret);
+  struct Color *ptr = convertColor(ret);
   ptr->red = saturateDouble(red, 0.0, 255.0);
   ptr->green = saturateDouble(green, 0.0, 255.0);
   ptr->blue = saturateDouble(blue, 0.0, 255.0);
   ptr->alpha = saturateDouble(alpha, 0.0, 255.0);
   return ret;
 }
-VALUE rb_color_new2() {
+VALUE rb_color_new2(void) {
   return rb_color_new(0.0, 0.0, 0.0, 0.0);
 }
 
 void rb_color_set(
     VALUE self, double red, double green, double blue, double alpha) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   ptr->red = saturateDouble(red, 0.0, 255.0);
   ptr->green = saturateDouble(green, 0.0, 255.0);
   ptr->blue = saturateDouble(blue, 0.0, 255.0);
@@ -32,8 +32,8 @@ void rb_color_set(
 }
 
 void rb_color_set2(VALUE self, VALUE other) {
-  Color *ptr = convertColor(self);
-  Color *other_ptr = convertColor(other);
+  struct Color *ptr = convertColor(self);
+  struct Color *other_ptr = convertColor(other);
   ptr->red = other_ptr->red;
   ptr->green = other_ptr->green;
   ptr->blue = other_ptr->blue;
@@ -41,35 +41,35 @@ void rb_color_set2(VALUE self, VALUE other) {
 }
 
 double rb_color_red(VALUE self) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   return ptr->red;
 }
 void rb_color_set_red(VALUE self, double red) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   ptr->red = red;
 }
 double rb_color_green(VALUE self) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   return ptr->green;
 }
 void rb_color_set_green(VALUE self, double green) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   ptr->green = green;
 }
 double rb_color_blue(VALUE self) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   return ptr->blue;
 }
 void rb_color_set_blue(VALUE self, double blue) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   ptr->blue = blue;
 }
 double rb_color_alpha(VALUE self) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   return ptr->alpha;
 }
 void rb_color_set_alpha(VALUE self, double alpha) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   ptr->alpha = alpha;
 }
 
@@ -91,56 +91,56 @@ static VALUE rb_color_m_old_load(VALUE self, VALUE s);
 static VALUE rb_color_m_old_dump(VALUE self, VALUE lim);
 
 VALUE rb_cColor;
-void Init_Color() {
+void Init_Color(void) {
   rb_cColor = rb_define_class("Color", rb_cObject);
   rb_define_alloc_func(rb_cColor, color_alloc);
   rb_define_method(rb_cColor, "initialize",
-      (VALUE(*)(...))rb_color_m_initialize, -1);
+      (VALUE(*)(ANYARGS))rb_color_m_initialize, -1);
   rb_define_method(rb_cColor, "initialize_copy",
-      (VALUE(*)(...))rb_color_m_initialize_copy, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_initialize_copy, 1);
   rb_define_method(rb_cColor, "set",
-      (VALUE(*)(...))rb_color_m_set, -1);
+      (VALUE(*)(ANYARGS))rb_color_m_set, -1);
   rb_define_method(rb_cColor, "red",
-      (VALUE(*)(...))rb_color_m_red, 0);
+      (VALUE(*)(ANYARGS))rb_color_m_red, 0);
   rb_define_method(rb_cColor, "red=",
-      (VALUE(*)(...))rb_color_m_set_red, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_set_red, 1);
   rb_define_method(rb_cColor, "green",
-      (VALUE(*)(...))rb_color_m_green, 0);
+      (VALUE(*)(ANYARGS))rb_color_m_green, 0);
   rb_define_method(rb_cColor, "green=",
-      (VALUE(*)(...))rb_color_m_set_green, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_set_green, 1);
   rb_define_method(rb_cColor, "blue",
-      (VALUE(*)(...))rb_color_m_blue, 0);
+      (VALUE(*)(ANYARGS))rb_color_m_blue, 0);
   rb_define_method(rb_cColor, "blue=",
-      (VALUE(*)(...))rb_color_m_set_blue, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_set_blue, 1);
   rb_define_method(rb_cColor, "alpha",
-      (VALUE(*)(...))rb_color_m_alpha, 0);
+      (VALUE(*)(ANYARGS))rb_color_m_alpha, 0);
   rb_define_method(rb_cColor, "alpha=",
-      (VALUE(*)(...))rb_color_m_set_alpha, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_set_alpha, 1);
 
   rb_define_method(rb_cColor, "to_s",
-      (VALUE(*)(...))rb_color_m_to_s, 0);
+      (VALUE(*)(ANYARGS))rb_color_m_to_s, 0);
   rb_define_singleton_method(rb_cColor, "_load",
-      (VALUE(*)(...))rb_color_m_old_load, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_old_load, 1);
   rb_define_method(rb_cColor, "_dump",
-      (VALUE(*)(...))rb_color_m_old_dump, 1);
+      (VALUE(*)(ANYARGS))rb_color_m_old_dump, 1);
 }
 
-Color *convertColor(VALUE obj) {
+struct Color *convertColor(VALUE obj) {
   Check_Type(obj, T_DATA);
   if(RDATA(obj)->dmark != (void(*)(void*))color_mark) {
     rb_raise(rb_eTypeError,
         "can't convert %s into Color",
         rb_class2name(rb_obj_class(obj)));
   }
-  Color *ret;
-  Data_Get_Struct(obj, Color, ret);
+  struct Color *ret;
+  Data_Get_Struct(obj, struct Color, ret);
   return ret;
 }
 
-static void color_mark(Color *) {}
+static void color_mark(struct Color *ptr) {}
 
 static VALUE color_alloc(VALUE klass) {
-  Color *ptr = ALLOC(Color);
+  struct Color *ptr = ALLOC(struct Color);
   VALUE ret = Data_Wrap_Struct(klass, color_mark, -1, ptr);
   return ret;
 }
@@ -248,7 +248,7 @@ static VALUE rb_color_m_set_alpha(VALUE self, VALUE alpha) {
   return alpha;
 }
 static VALUE rb_color_m_to_s(VALUE self) {
-  Color *ptr = convertColor(self);
+  struct Color *ptr = convertColor(self);
   char s[50];
   snprintf(s, sizeof(s), "(%f, %f, %f, %f)",
       ptr->red,
@@ -257,9 +257,9 @@ static VALUE rb_color_m_to_s(VALUE self) {
       ptr->alpha);
   return rb_str_new2(s);
 }
-static VALUE rb_color_m_old_load(VALUE, VALUE str) {
+static VALUE rb_color_m_old_load(VALUE klass, VALUE str) {
   VALUE ret = color_alloc(rb_cColor);
-  Color *ptr = convertColor(ret);
+  struct Color *ptr = convertColor(ret);
   char *s = StringValuePtr(str);
   if(!s) return ret;
   ptr->red = saturateDouble(readDouble(s), 0.0, 255.0);
@@ -268,8 +268,8 @@ static VALUE rb_color_m_old_load(VALUE, VALUE str) {
   ptr->alpha = saturateDouble(readDouble(s+24), 0.0, 255.0);
   return ret;
 }
-static VALUE rb_color_m_old_dump(VALUE self, VALUE) {
-  Color *ptr = convertColor(self);
+static VALUE rb_color_m_old_dump(VALUE self, VALUE limit) {
+  struct Color *ptr = convertColor(self);
   char s[32];
   writeDouble(s, ptr->red);
   writeDouble(s+8, ptr->green);
--- accordion/Color.h
+++ accordion/Color.h
@@ -3,11 +3,15 @@
 
 #include <ruby.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern VALUE rb_cColor;
-extern void Init_Color();
+extern void Init_Color(void);
 
 VALUE rb_color_new(double red, double green, double blue, double alpha);
-VALUE rb_color_new2();
+VALUE rb_color_new2(void);
 
 void rb_color_set(VALUE self, double red, double green, double blue, double alpha);
 void rb_color_set2(VALUE self, VALUE other);
@@ -19,5 +23,9 @@ double rb_color_blue(VALUE self);
 void rb_color_set_blue(VALUE self, double blue);
 double rb_color_alpha(VALUE self);
 void rb_color_set_alpha(VALUE self, double alpha);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // COLOR_H_INCLUDED
